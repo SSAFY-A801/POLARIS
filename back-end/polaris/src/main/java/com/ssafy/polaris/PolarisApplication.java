8c63b32b851c4b2a92d5578d0d14fd262faa6862
황진하 <hjh70902000@naver.com>
2024-01-17 13:24:51 +0900

    S10P12A801-52 Init : backend 초기 세팅

---
+++ back-end/polaris/src/main/java/com/ssafy/polaris/PolarisApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.polaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarisApplication.class, args);
+	}
+
+}
