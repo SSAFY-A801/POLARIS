b6312a84b570ac7c0660f7f397334053dbb1d907
권원영 <onyo9@naver.com>
2024-01-29 17:57:02 +0900

    Revert "Merge branch 'feature-FE/profile' into 'develop'"
    This reverts merge request !8

--- front-end/polaris/src/router/index.ts
+++ front-end/polaris/src/router/index.ts
@@ -1,100 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ProfilePage from '@/views/profile/ProfilePage.vue'
-import MyLibraryPage from '@/views/profile/mylibrary/MyLibraryPage.vue'
-import MyScrapsPage from '@/views/profile/like/MyScrapsPage.vue'
-import MyFavoritesPage from '@/views/profile/like/MyFavoritesPage.vue'
-import MyEssayPage from '@/views/profile/myarticle/MyEssayPage.vue'
-import MyPromotionPage from '@/views/profile/myarticle/MyPromotionPage.vue'
-import MyArticlePage from '@/views/profile/myarticle/MyArticlePage.vue'
-import ProfileUpdatePage from '@/views/profile/userinfo/ProfileUpdatePage.vue'
-import PasswordChangePage from '@/views/profile/userinfo/PasswordChangePage.vue'
-import BookRegisterPage from '@/views/profile/mylibrary/BookRegisterPage.vue'
-import MyTradeListPage from '@/views/profile/mytradeinfo/MyTradeListPage.vue'
-import MyExchangeListPage from '@/views/profile/mytradeinfo/MyExchangeListPage.vue'
-import FollowingListPage from '@/views/profile/FollowingListPage.vue'
-import BookDetailPage from '@/views/profile/mylibrary/BookDetailPage.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // Profile
     {
-      path: '/profile',
-      redirect: '/profile/library',
-      name: 'ProfilePage',
-      component: ProfilePage,
-      children: [
-        { path: 'library',
-          name: 'MyLibraryPage',
-          component: MyLibraryPage,
-        },
-        { path: 'myscraps',
-          name: 'MyScrapsPage',
-          component: MyScrapsPage 
-        },
-        { path: 'myfavorites',
-          name: 'MyFavoritesPage',
-          component: MyFavoritesPage
-         },
-        { path: 'myarticle',
-          redirect: '/profile/myarticle/myessay',
-          name: 'MyArticlePage', 
-          component: MyArticlePage, 
-          // 나의독후감, 나의홍보게시글
-          children: [
-            { path: 'myessay',
-            name: 'MyEssayPage',
-            component: MyEssayPage 
-          },
-          { path: 'myscraps',
-            name: 'MyPromotionPage',
-            component: MyPromotionPage 
-          },
-          ]
-        },
-      ],
+      path: '/',
+      name: 'home',
+      component: HomeView
     },
-    // Profile 수정
-    { 
-      path: '/profile/update',
-      name: 'ProfileUpdatePage',
-      component: ProfileUpdatePage, 
-    },
-    // Password 변경
-    { 
-      path: '/passwordchange',
-      name: 'PasswordChangePage',
-      component: PasswordChangePage, 
-    },
-
-    // 서재에 도서등록 => 모달 제작이 되면 없앨 예정
-    { 
-      path: '/registerbook',
-      name: 'BookRegisterPage',
-      component: BookRegisterPage,
-    },
-    // 거래내역 확인
-    { 
-      path: '/profile/trade-books',
-      name: 'MyTradeListPage',
-      component: MyTradeListPage,
-    },
-    { 
-      path: '/profile/exchange-books',
-      name: 'MyExchangeListPage',
-      component: MyExchangeListPage,
-    },
-    // 팔로잉 목록 확인
-    { 
-      path: '/profile/follow',
-      name: 'FollowingListPage',
-      component: FollowingListPage,
-    },
-    // 사용자 도서 상세 페이지
     {
-      path: '/bookdetail',
-      name: 'BookDetailPage',
-      component: BookDetailPage,
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
     }
   ]
 })
