1897bb49a90f896fbacc4167a891e2019c08d2e1
황진하 <hjh70902000@naver.com>
2024-01-29 17:37:13 +0900

    [POLARIS] BE Fix : User response 추가

---
+++ back-end/polaris/src/main/java/com/ssafy/polaris/user/response/StatusCode.java
@@ -0,0 +1,13 @@
+package com.ssafy.polaris.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum StatusCode {
+	SUCCESS_LOGIN(200, "로그인 성공");
+
+	private final int status;
+	private final String message;
+}
