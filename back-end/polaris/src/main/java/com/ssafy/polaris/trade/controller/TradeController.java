89d26cf0e5c7556e2d9b28950b4ad7a4344cafea
seomiii <seomiii1765@gmail.com>
2024-01-30 15:15:24 +0900

    [POLARIS S10P12A801-234] BE Feat : 거래 가능 사용자 도서 목록 api 구현 판매 가능 사용자 도서 목록 , 교환 가능 사용자 도서 목록 api 구현 및 패키지 변경

--- back-end/polaris/src/main/java/com/ssafy/polaris/trade/controller/TradeController.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/trade/controller/TradeController.java
@@ -1,15 +1,59 @@
 package com.ssafy.polaris.trade.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.ssafy.polaris.common.DefaultResponse;
+import com.ssafy.polaris.trade.dto.TradeBookListResponseDto;
+import com.ssafy.polaris.trade.dto.TradeBookResponseDto;
+import com.ssafy.polaris.trade.response.DefaultResponse;
+import com.ssafy.polaris.trade.response.StatusCode;
+import com.ssafy.polaris.trade.service.TradeService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class TradeController {
+	private final TradeService tradeService;
 
-	// @GetMapping(path = "/exchange_books")
-	// public ResponseEntity<DefaultResponse>
+	/**
+	 * 교환 가능 도서 목록 반환
+	 * @return
+	 */
+	@GetMapping(path = "/exchange_books")
+	public ResponseEntity<DefaultResponse<TradeBookListResponseDto>> getExchangeBookList(){
+		// TODO : userId 토큰에서 가져오기
+		Long userId = 1L;
+		List<TradeBookResponseDto> exchangeBookResponseDtoList = tradeService.getExchangeBookList(userId);
+
+		TradeBookListResponseDto exchangeBookListResponseDto = new TradeBookListResponseDto(userId, exchangeBookResponseDtoList);
+		return DefaultResponse.toResponseEntity(
+			HttpStatus.OK,
+			StatusCode.SUCCESS_VIEW,
+			exchangeBookListResponseDto
+		);
+	}
+
+	/**
+	 * 판매 가능 사용자 도서 목록 반환
+	 * @return
+	 */
+	@GetMapping(path = "/purchase_books")
+	public ResponseEntity<DefaultResponse<TradeBookListResponseDto>> getPurchaseBookList(){
+		// TODO : userId 토큰에서 가져오기
+		Long userId = 1L;
+		List<TradeBookResponseDto> purchaseBookResponseDtoList = tradeService.getPurchaseBookList(userId);
+
+		TradeBookListResponseDto purchaseBookListResponseDto = new TradeBookListResponseDto(userId, purchaseBookResponseDtoList);
+		return DefaultResponse.toResponseEntity(
+			HttpStatus.OK,
+			StatusCode.SUCCESS_VIEW,
+			purchaseBookListResponseDto
+		);
+	}
 
 }
--- back-end/polaris/src/main/java/com/ssafy/polaris/trade/service/TradeServiceImpl.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/trade/service/TradeServiceImpl.java
@@ -1,4 +1,25 @@
 package com.ssafy.polaris.trade.service;
 
-public class TradeServiceImpl{
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.ssafy.polaris.book.domain.UserBookTradeType;
+import com.ssafy.polaris.book.repository.UserBookRepository;
+import com.ssafy.polaris.trade.dto.TradeBookResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TradeServiceImpl implements TradeService{
+	private final UserBookRepository userBookRepository;
+	@Override
+	public List<TradeBookResponseDto> getPurchaseBookList(Long userId) {
+		return userBookRepository.getTradeBookList(userId, UserBookTradeType.PURCHASE);
+	}
+
+	@Override
+	public List<TradeBookResponseDto> getExchangeBookList(Long userId) {
+		return userBookRepository.getTradeBookList(userId, UserBookTradeType.EXCHANGE);
+	}
 }
