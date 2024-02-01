98503f347a7adb43a918e2db740e2a8de9e95102
seomiii <seomiii1765@gmail.com>
2024-02-01 12:58:29 +0900

    [POLARIS] BE Fix : Trade id 자료형 Long으로 변경

--- back-end/polaris/src/main/java/com/ssafy/polaris/chat/repository/TradeRepository.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/chat/repository/TradeRepository.java
@@ -3,7 +3,7 @@ package com.ssafy.polaris.chat.repository;
 import com.ssafy.polaris.trade.domain.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TradeRepository extends JpaRepository<Trade, Integer> {
+public interface TradeRepository extends JpaRepository<Trade, Long> {
 	// 나의 채팅방 목록
 	// List<Trade> findAllByUser(User user);
 }
