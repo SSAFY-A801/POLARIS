85478e17c6a52efe6d755ad10b3e4e2f3729f237
김승우 <wskyard96@gmail.com>
2024-02-01 16:25:08 +0900

    [POLARIS] BE Feat : 지역 가져오는 API

---
+++ back-end/polaris/src/main/java/com/ssafy/polaris/regcode/dto/RegcodeResponseDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.polaris.regcode.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegcodeResponseDto {
+    Long code;
+    String name;
+}
