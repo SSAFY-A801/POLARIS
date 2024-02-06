9945d1d797e3b111efd155e5b5a4cd754cbdf852
황진하 <hjh70902000@naver.com>
2024-02-06 17:53:15 +0900

    [POLARIS] BE Fix : 홍보글용 전역 예외 수정

--- back-end/polaris/src/main/java/com/ssafy/polaris/global/exception/response/ErrorCode.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/global/exception/response/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
 	NO_BOOK_SELECTED(400, "책이 선택되지 않았습니다."),
 	USER_BOOK_NOT_EXIST(400, "존재하지 않는 사용자 도서입니다."),
 
-	NOT_FOUND(404, "존재하지 않는 사용자 도서입니다.")
+	NOT_FOUND(404, "리소스를 찾을 수 없습니다.")
 	;
 	private final int status;
 	private final String message;
