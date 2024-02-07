164edac5aaf83c815108dd55a7d96e2cd9ab23b2
onyo9 <onyo9ssafy@gmail.com>
2024-02-07 10:45:20 +0900

    [POLARIS S10P12A801-342] FE Fix: 헤더 동적 라우팅 오류 수정

--- front-end/polaris/src/components/common/Header.vue
+++ front-end/polaris/src/components/common/Header.vue
@@ -58,8 +58,22 @@ import { useRouter } from 'vue-router'
 // const props = defineProps(['userToken'])
 const userToken = ref(localStorage.getItem('user_token'))
 const userInfoString = ref<string>(localStorage.getItem('user_info') ?? "");
-const loginUser = JSON.parse(userInfoString.value)
+// 사용자 정보를 나타내는 인터페이스 정의
+interface UserInfo {
+  id?: string | null;
+  // 다른 속성들도 필요에 따라 추가할 수 있습니다.
+}
 
+// userInfoString에서 가져온 값을 파싱하여 UserInfo 타입으로 사용
+let loginUser: UserInfo;
+
+try {
+  loginUser = JSON.parse(userInfoString.value) || {}; // 빈 객체로 기본값 설정
+} catch (error) {
+  console.error("Error parsing user_info:", error);
+  loginUser = {}; // JSON 파싱에 실패한 경우 빈 객체로 기본값 설정
+}
+// loginUser를 사용할 때 loginUser.id를 체크할 때 에러가 발생하지 않습니다.
 
 
 const router = useRouter()
