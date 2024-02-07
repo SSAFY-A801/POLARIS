4b9541bcb155e6b789a4f5661a78a4523db9fcac
onyo9 <onyo9ssafy@gmail.com>
2024-02-08 01:53:02 +0900

    [POLARIS S10P12A801-344] FE Feat: 독후감 게시판 목록 페이지 및 상세 페이지 구성

---
+++ front-end/polaris/src/components/essay/EssayList.vue
@@ -0,0 +1,16 @@
+<template>
+  <EssayListItem
+  v-for="(essay, index) in EssayList"
+  :key = "index"
+  :essayInfo = "essay"
+  />
+</template>
+
+<script setup lang="ts">
+  import EssayListItem from "./EssayListItem.vue";
+  const EssayList = 4;
+</script>
+
+<style scoped>
+
+</style>
