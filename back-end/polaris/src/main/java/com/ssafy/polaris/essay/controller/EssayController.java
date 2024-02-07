c828768f5807743baa1bfb31206b66e8f73513b9
황진하 <hjh70902000@naver.com>
2024-02-07 17:11:39 +0900

    [POLARIS] BE Fix : CORS 불필요한 설정 삭제

--- back-end/polaris/src/main/java/com/ssafy/polaris/essay/controller/EssayController.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/essay/controller/EssayController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -27,7 +26,6 @@ import com.ssafy.polaris.global.security.SecurityUser;
 
 import lombok.RequiredArgsConstructor;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/essay")
 @RequiredArgsConstructor
--- back-end/polaris/src/main/java/com/ssafy/polaris/promotion/controller/PromotionController.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/promotion/controller/PromotionController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,7 +27,6 @@ import com.ssafy.polaris.promotion.service.PromotionService;
 
 import lombok.RequiredArgsConstructor;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/promotion")
 @RequiredArgsConstructor
