ba1dca64fef6d2f45bb398e0542790abbe962c70
김승우 <wskyard96@gmail.com>
2024-02-12 15:03:45 +0900

    [POLARIS] BE Feat : 나의 스크랩 리스트 조회 시 책 제목도 같이 리턴하도록 변경

--- back-end/polaris/src/main/java/com/ssafy/polaris/essay/repository/ScrapRepository.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/essay/repository/ScrapRepository.java
@@ -32,9 +32,10 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 	// void deleteWithEssayIdAndUserId(@Param("essayId") Long essayId, @Param("userId") Long userId);
 
 	@Query("select new com.ssafy.polaris.essay.dto.ScrappedEssayByUserResponseDto(" +
-			" e.id, e.title, u.id, u.nickname, e.createdAt) from Scrap s " +
+			" e.id, e.title, u.id, u.nickname, b.title, e.createdAt) from Scrap s " +
 			" left join s.user u " +
 			" left join s.essay e " +
+			" left join e.userBook.book b" +
 			" where s.user.id = :userId")
 	List<ScrappedEssayByUserResponseDto> getUserScrappedList(@Param("userId") Long userId);
 }
