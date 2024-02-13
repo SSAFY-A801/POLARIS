761e6db28d0f17826c9051f71ff0505b5dfa9eeb
seomiii <seomiii1765@gmail.com>
2024-02-13 16:26:58 +0900

    [POLARIS] BE Fix : 채팅방 참여자 조회 api response data 수정

--- back-end/polaris/src/main/java/com/ssafy/polaris/chat/repository/TradeRepository.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/chat/repository/TradeRepository.java
@@ -45,6 +45,8 @@ public interface TradeRepository extends JpaRepository<Trade, Long> {
 	"SELECT " +
 		"new com.ssafy.polaris.chat.dto.ChatRoomParticipantsResponseDto( " +
 		"t.id AS chatRoomId, "+
+		"t.sender.id AS tradeSenderId, "+
+		"t.receiver.id AS tradeReceiverId, "+
 		":userId AS senderId, "+
 		"u.id AS receiverId, "+
 		"u.nickname AS receiverNickname, "+
@@ -58,6 +60,8 @@ public interface TradeRepository extends JpaRepository<Trade, Long> {
 		"SELECT " +
 		"new com.ssafy.polaris.chat.dto.ChatRoomParticipantsResponseDto( " +
 		"t.id AS chatRoomId, "+
+		"t.sender.id AS tradeSenderId, "+
+		"t.receiver.id AS tradeReceiverId, "+
 		":userId AS senderId, "+
 		"u.id AS receiverId, "+
 		"u.nickname AS receiverNickname, "+
