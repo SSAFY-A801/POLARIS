7d7a43b5fae0ac95ea88d4b6cb294149619d706d
Jinha Hwang <hjh70902000@gmail.com>
2024-02-13 10:33:27 +0900

    [POLARIS] BE Fix : 에러 메시지 출력값 실수 수정

--- back-end/polaris/src/main/java/com/ssafy/polaris/user/exception/UserNotKakaoJoined.java
+++ back-end/polaris/src/main/java/com/ssafy/polaris/user/exception/UserNotKakaoJoined.java
@@ -6,6 +6,6 @@ import com.ssafy.polaris.global.exception.response.ErrorCode;
 public class UserNotKakaoJoined extends UnAuthorizedException {
 	private static final ErrorCode errorCode = ErrorCode.USER_NOT_KAKAO_JOINED;
 	public UserNotKakaoJoined(String detailMessageKey, Object... params) {
-		super(MESSAGE_KEY + " : " + detailMessageKey, errorCode, params);
+		super(errorCode.getMessage() + " : " + detailMessageKey, errorCode, params);
 	}
 }
